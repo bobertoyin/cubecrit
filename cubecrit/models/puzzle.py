f38de5fc39b042d298a43123eca864b379f3bbc9
Eli Parker <epproper54@gmail.com>
2024-03-02 22:11:20 -0600

    Added the remaining tables, Added data classes for countries and manufacturers

--- cubecrit/models/puzzle.py
+++ cubecrit/models/puzzle.py
@@ -24,6 +24,60 @@ class PuzzleType:
 
 
 @dataclass(frozen=True)
+class Country:
+    external_id: str
+    display_name: str
+
+    @staticmethod
+    def get_country(conn: Connection, external_id: str) -> Optional["Country"]:
+        result = conn.execute(
+            text(
+                "SELECT external_id, display_name FROM countries WHERE external_id = :external_id"
+            ),
+            {"external_id": external_id},
+        ).first()
+        conn.commit()
+        if result is not None:
+            return Country(**result._asdict())
+        return None
+
+
+@dataclass(frozen=True)
+class Manufacturer:
+    external_id: str
+    display_name: str
+    country: Country
+
+    @staticmethod
+    def get_manufacturer(
+        conn: Connection, external_id: str
+    ) -> Optional["Manufacturer"]:
+        result = conn.execute(
+            text(
+                """SELECT manufacturers.external_id, countries.external_id as countries_external_id,
+                    manufacturers.display_name, countries.display_name as countries_display_name
+                    FROM manufacturers
+                    JOIN countries ON manufacturers.country_id = countries.id
+                    WHERE manufacturers.external_id = :external_id
+                    """
+            ),
+            {"external_id": external_id},
+        ).first()
+        conn.commit()
+        if result is not None:
+            country = Country(
+                external_id=result.countries_external_id,
+                display_name=result.countries_display_name,
+            )
+            return Manufacturer(
+                country=country,
+                external_id=result.external_id,
+                display_name=result.display_name,
+            )
+        return None
+
+
+@dataclass(frozen=True)
 class Puzzle:
     external_id: str
     display_name: str
@@ -39,7 +93,8 @@ class Puzzle:
                         puzzles.display_name, puzzle_types.display_name as puzzle_types_display_name, 
                         release_date, discontinue_date FROM puzzles 
                 JOIN puzzle_types 
-                ON puzzles.puzzle_type_id = puzzle_types.id"""
+                ON puzzles.puzzle_type_id = puzzle_types.id
+                WHERE puzzles.external_id = :external_id"""
             ),
             {"external_id": external_id},
         ).first()
@@ -57,3 +112,7 @@ class Puzzle:
                 discontinue_date=result.discontinue_date,
             )
         return None
+
+
+# MAKE USERS CLASS AND METHOD
+# MAKE REVIEWS CLASS AND METHOD
--- cubecrit/schema.sql
+++ cubecrit/schema.sql
@@ -4,6 +4,23 @@ CREATE TABLE IF NOT EXISTS puzzle_types (
     display_name VARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS countries (
+    id SERIAL PRIMARY KEY,
+    external_id VARCHAR UNIQUE NOT NULL,
+    display_name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS manufacturers (
+    id SERIAL PRIMARY KEY,
+    external_id VARCHAR UNIQUE NOT NULL,
+    display_name VARCHAR NOT NULL,
+    country_id INT UNIQUE NOT NULL,
+    -- picture TEXT
+    CONSTRAINT fk_country
+        FOREIGN KEY (country_id)
+        REFERENCES countries(id)
+);
+
 CREATE TABLE IF NOT EXISTS puzzles (
     id SERIAL PRIMARY KEY,
     external_id VARCHAR UNIQUE NOT NULL,
@@ -11,11 +28,53 @@ CREATE TABLE IF NOT EXISTS puzzles (
     release_date DATE,
     discontinue_date DATE,
     puzzle_type_id INT NOT NULL,
+    manufacturer_id INT NOT NULL,
     -- picture TEXT,
     CONSTRAINT fk_puzzle_type
         FOREIGN KEY(puzzle_type_id)
-        REFERENCES puzzle_types(id)
+        REFERENCES puzzle_types(id),
+    CONSTRAINT fk_manufacturer
+        FOREIGN KEY(manufacturer_id)
+        REFERENCES manufacturers(id)
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    wca_id VARCHAR UNIQUE NOT NULL,
+    joined TIMESTAMP DEFAULT NOW(),
+    first_name VARCHAR,
+    last_name VARCHAR
+    -- profile_picture TEXT
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    puzzle_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    rating INT NOT NULL,
+    content TEXT,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id),
+    CONSTRAINT fk_puzzle
+        FOREIGN KEY (puzzle_id)
+        REFERENCES puzzles(id)
+);
+
+INSERT INTO countries(external_id, display_name)
+VALUES ('china', 'China')
+ON CONFLICT(external_id)
+DO UPDATE SET
+    external_id = EXCLUDED.external_id;
+
+INSERT INTO manufacturers(external_id, display_name, country_id)
+VALUES ('moyu', 'MoYu', (SELECT id FROM countries
+    WHERE countries.external_id = 'china'))
+ON CONFLICT(external_id)
+DO UPDATE SET
+    external_id = EXCLUDED.external_id;
 
 INSERT INTO puzzle_types(external_id, display_name)
 VALUES ('3x3', '3x3')
@@ -23,9 +82,12 @@ ON CONFLICT(external_id)
 DO UPDATE SET
     external_id = EXCLUDED.external_id;
 
-INSERT INTO puzzles(external_id, display_name, release_date, discontinue_date, puzzle_type_id)
-VALUES ('aolong-v2', 'AoLong V2', '2014-06-01', '2018-06-01', (SELECT id from puzzle_types
-    WHERE puzzle_types.external_id = '3x3'))
+INSERT INTO puzzles(external_id, display_name, release_date, discontinue_date, puzzle_type_id, manufacturer_id)
+VALUES ('aolong-v2', 'AoLong V2', '2014-06-01', '2018-06-01', 
+    (SELECT id FROM puzzle_types
+        WHERE puzzle_types.external_id = '3x3'), 
+    (SELECT id FROM manufacturers
+        WHERE manufacturers.external_id = 'moyu'))
 ON CONFLICT(external_id)
 DO UPDATE SET
     external_id = EXCLUDED.external_id;
