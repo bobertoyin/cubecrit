fc1a5df38369643f523f581cb7bca0750440bf92
Robert Yin <bobertoyin@gmail.com>
2024-03-04 22:28:29 -0600

    reset unit test db twice in fixture, add more test cases to test_get_puzzle_type

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,19 +9,19 @@ engine = create_engine("postgresql://test:password@localhost:5433/test")
 @fixture
 def db_connection() -> Generator[Connection, Any, Any]:
     connection = engine.connect()
+    reset_query = text(
+        """
+        SELECT 'DROP TABLE IF EXISTS "' || tablename || '" CASCADE;' FROM
+        pg_tables WHERE schemaname = 'public';
+        """
+    )
+    connection.execute(reset_query)
     with open("cubecrit/schema.sql") as schema:
         connection.execute(text(schema.read()))
     with open("cubecrit/seed.sql") as seed:
         connection.execute(text(seed.read()))
     connection.commit()
     yield connection
-    connection.execute(
-        text(
-            """
-            SELECT 'DROP TABLE IF EXISTS "' || tablename || '" CASCADE;' FROM
-            pg_tables WHERE schemaname = 'public';
-            """
-        )
-    )
+    connection.execute(reset_query)
     connection.commit()
     connection.close()
--- tests/models/test_puzzle.py
+++ tests/models/test_puzzle.py
@@ -5,7 +5,13 @@ from cubecrit.models.puzzle import PuzzleType
 
 
 @mark.parametrize(
-    "external_id, expected", [("3x3", PuzzleType("3x3", "3x3")), ("-1x-1", None)]
+    "external_id, expected",
+    [
+        ("3x3", PuzzleType("3x3", "3x3")),
+        ("-1x-1", None),
+        ("megaminx", PuzzleType("megaminx", "Megaminx")),
+        ("Megaminx", None),
+    ],
 )
 def test_get_puzzle_type(
     db_connection: Connection, external_id: str, expected: PuzzleType | None
