0d7ee4fe485528d8f8f3129df938e84ca935b571
Eli Parker <epproper54@gmail.com>
2024-04-14 23:43:21 -0500

    Fixing lint erros

--- cubecrit/sql/get_review.sql
+++ cubecrit/sql/get_review.sql
@@ -1,27 +1,23 @@
-SELECT 
+SELECT
     reviews.created_at,
     reviews.updated_at,
     reviews.rating,
     reviews.content,
-
-    users.wca_id as users_wca_id,
-    users.joined as users_joined, 
-    users.first_name as users_first_name,
-    users.last_name as users_last_name,
-    users.profile_picture_url as users_profile_picture_url,
-
-    puzzles.external_id as puzzles_external_id,
+    users.wca_id AS users_wca_id,
+    users.joined AS users_joined,
+    users.first_name AS users_first_name,
+    users.last_name AS users_last_name,
+    users.profile_picture_url AS users_profile_picture_url,
+    puzzles.external_id AS puzzles_external_id,
     puzzle_types.external_id AS puzzle_types_external_id,
-    puzzles.display_name as puzzles_display_name,
+    puzzles.display_name AS puzzles_display_name,
     puzzle_types.display_name AS puzzle_types_display_name,
-    puzzles.release_date as puzzles_release_date,
-    puzzles.discontinue_date as puzzles_discontinue_date,
-
+    puzzles.release_date AS puzzles_release_date,
+    puzzles.discontinue_date AS puzzles_discontinue_date,
     manufacturers.external_id AS manufacturers_external_id,
     manufacturers.display_name AS manufacturers_display_name,
     countries.external_id AS countries_external_id,
     countries.display_name AS countries_display_name
-    
 INNER JOIN users
     ON reviews.user_id = users.id 
 INNER JOIN puzzles
@@ -32,6 +28,6 @@ INNER JOIN manufacturers
     ON puzzles.manufacturer_id = manufacturers.id
 INNER JOIN countries
     ON manufacturers.country_id = countries.id
-
-WHERE users_wca_id = :wca_id
+WHERE
+    users_wca_id = :wca_id
     AND puzzles.external_id = :puzzles_external_id;
