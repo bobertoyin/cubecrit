1a6c2995b14a4b1486d37f5e0bb49c79ca35c07f
Eli Parker <epproper54@gmail.com>
2024-09-14 11:29:22 -0500

    Implement validation functions

--- cubecrit/controllers/search.py
+++ cubecrit/controllers/search.py
@@ -1,8 +1,8 @@
 """Routes for search pages."""
 from flask import Blueprint, abort, current_app, render_template, request
 
-from ..controllers.puzzles import validate_page_number
 from ..models.puzzle import Puzzle, PuzzleType
+from ..validate import validate_page_number, validate_query
 
 search = Blueprint(
     "search", __name__, template_folder="templates", url_prefix="/search"
@@ -22,11 +22,7 @@ def get_search_route() -> str:
     page = validate_page_number(request.args.get("page"))
     if puzzle_type is not None and puzzle_type.strip() == "":
         puzzle_type = None
-    query = request.args.get("query")
-    if query is not None:
-        query = query.strip()
-        if query == "":
-            query = None
+    query = validate_query(request.args.get("query"))
     with current_app.config["db"].connect() as connection:
         all_puzzle_types = PuzzleType.get_all_puzzle_types(connection)
         num_pages = Puzzle.get_num_pages(connection, query, puzzle_type)
